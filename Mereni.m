4ff54c3085c20f5922f7d4157764418d039b0718
Juraj Lieskovský <lieskovsky.juraj@gmail.com>
2021-03-28 13:00:23 +0200

    minor

--- Mereni.m
+++ Mereni.m
@@ -3,7 +3,7 @@ addpath('fittingTools')
 
 % Dobeh
 DB = table2array(readtable('Mereni/Dobeh.CSV','range',[19,4]));
-fitLin(-24.62,[],DB(:,1),DB(:,2),true)
+[~,a1] = fitLin(-24.62,[],DB(:,1),DB(:,2),true);
 
 % Zatezovani
 rozsahy = ["A3:F13","J3:O6","J10:O14"];
@@ -13,11 +13,12 @@ for i = 1:numel(rozsahy)
 	z{i}.om = 80/1000*30/pi*Z(:,2);
 	z{i}.I = Z(:,3);
 	z{i}.U = Z(:,4);
-	z{i}.P = Z(:,6).*Z(:,5);
+	z{i}.alfa = Z(:,5);
+	z{i}.kW = Z(:,6);
 end
 z{1}.R2 = 0; %Ohm
-z{2}.R2 = 0;
-z{3}.R3 = 0.67;
+z{2}.R2 = 0; %Ohm
+z{3}.R2 = 0.67; %Ohm
 
 % Naprazdno a nakratko
 NP = table2cell(readtable('mereni_AS_motor.xlsx','range','B21:C24')); NP_T = NP';
