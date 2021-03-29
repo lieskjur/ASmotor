9d7c9e5f1b22db542ec0a12be0f3bf2227d5dfda
Juraj Lieskovský <lieskovsky.juraj@gmail.com>
2021-03-29 23:12:32 +0200

    madness

---
+++ OptFun.m
@@ -0,0 +1,46 @@
+function COST = OptFun(teor,mer,podm,delta,plotORnot)
+	if nargin < 5; plotORnot = false; end;
+
+	%% UPRAVA PARAMETRU
+	pn = fieldnames(teor);
+	for i = 1:numel(pn)
+		par.(pn(i)) = teor.(pn(i))+delta(i);
+	end
+
+	% Simulace
+	ODE_fun = @(t,y) ASmotor(par,podm,t,y);
+	[T,Y] = ode45(ODE_fun,podm.t,zeros(5,1));
+	for i = 1:length(T)
+		[dY(i,:),A(i,:)] = ODE_fun(T(i),Y(i,:));
+	end
+
+	% Formatovani vystupu simulace
+	sim.i_1 = A
+	sim.om = dY(:,7);
+	sim.t = T;
+
+	% Cost funkce
+	[sm,mr] = find( sim.t' == mer.t );
+	rn = ["om","i_1"];
+	for i = 1:numel(rn)
+		sim_fld = sim.(rn(i))(sm);
+		mer_fld = mer.(rn(i))(in);
+		Delta(i) = norm(sim_fld-mer_fld);
+		Ny(i) = mean([sim_fld;mer_fld]);
+	end
+	COST = Delta'*diag(1./Ny)*Delta;
+
+	% Vizualizace
+	if plotORnot == true
+		figure; hold on; c = 0; %>>
+		for i = ["mer","sim"]
+			for j = ["om","i_1"]
+				fld = eval(i+"."+j);
+				plot(fld(:,1),fld(:,2))
+				c = c+1;
+				lgd(c) = j+" - "+i;
+			end
+		end %<<
+	end
+
+end
--- TeorParam.m
+++ TeorParam.m
@@ -6,16 +6,8 @@ DB = table2array(readtable('Mereni/Dobeh.CSV','range',[19,4]));
 [~,dwdt] = fitLin(-24.62,[],DB(:,1),DB(:,2),true);
 r = 256e-3; %m
 G = 1860e-3*9.81; %N
-M_B = r*G;
-J = 1/(dwdt*M_B)
-
-%% Rozbeh
-U = 124;
-%I_ef = 4.5; %A NEVYCHAZI
-rb1.om = table2array(readtable('Mereni/Rozbeh1CH1.CSV','range',[19,4]))' .* [1;80/1000*30/pi];
-rb1.i = table2array(readtable('Mereni/Rozbeh1CH2.CSV','range',[19,4]))' .* [1;11/(1.085*sqrt(2))]; % 11A/1.085V; ef hodnota
-plot(rb1.om(1,:),rb1.om(2,:))
-plot(rb1.i(1,:),rb1.i(2,:))
+teor.M_B = r*G;
+teor.J = 1/(dwdt*M_B)
 
 % Naprazdno a nakratko
 NP = table2cell(readtable('mereni_AS_motor.xlsx','range','B21:C24')); NP_T = NP';
@@ -27,3 +19,5 @@ nk = struct(NK_T{:});
 R1 = 0.888;
 R2 = 0.191;
 p12 = 0.46;
+
+save('TeorParam.mat','-struct','teor')
