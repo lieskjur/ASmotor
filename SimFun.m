62ef9955e8bf7d2171ee479c1cef402162765413
Juraj Lieskovský <lieskovsky.juraj@gmail.com>
2021-04-11 17:41:33 +0200

    Spustitelné

--- SimFun.m
+++ SimFun.m
@@ -1,4 +1,4 @@
-function [sim,param] = SimFun(teor,mer,podm,delta,plotORnot)
+function [sml,param] = SimFun(teor,mer,podm,delta,plotORnot)
 	if nargin < 5; plotORnot = false; end;
 
 	% Uprava parametru
@@ -8,21 +8,33 @@ function [sim,param] = SimFun(teor,mer,podm,delta,plotORnot)
 	end
 
 	% Simulace
-	ODE_fun = @(t,y) ASmotor(param,podm,t,y);
-	[T,Y] = ode45(ODE_fun,podm.t,zeros(5,1));
-	for i = 1:length(T)
-		[dY(i,:),A(i,:)] = ODE_fun(T(i),Y(i,:));
-	end
+	% ODE_fun = @(t,y) ASmotor(param,podm,t,y);
+	% [T,Y] = ode45(ODE_fun,podm.t,zeros(5,1));
+	% for i = 1:length(T)
+	% 	[dY(i,:),A(i,:)] = ODE_fun(T(i),Y(i,:));
+	% end
+
+	Ls = param.L1;
+	Lr = param.L2;
+	Lh = param.Lh;
+	pp = param.pp;
+	Rs = param.R1;
+	Rr = param.R2;
+	J = param.J;
+	Umax = podm.Umax;
+
+	options = simset('SrcWorkspace','current');
+	SimOut = sim('ASmodelViskozni',[],options);
 
 	% Formatovani vystupu simulace
-	sim.i_ef = A/(sqrt(2)*3);
-	sim.om = Y(:,5);
-	sim.t = T;
+	sml.i_ef = reshape(SimOut.I.Data/sqrt(2),1,[]);
+	sml.om = reshape(SimOut.om.Data/sqrt(2),1,[]);
+	sml.t = SimOut.tout;
 
 	% Vizualizace
 	if plotORnot == true
 		figure; hold on; %>>
-		for i = ["mer","sim"]
+		for i = ["mer","sml"]
 			time = eval(i+".t");
 			for j = ["om","i_ef"]
 				if j == "om"; yyaxis left; end
@@ -31,7 +43,7 @@ function [sim,param] = SimFun(teor,mer,podm,delta,plotORnot)
 				plot(time,fld)
 			end
 		end
-		legend(["\omega - mer","\omega - sim","i_{ef} - mer","i_{ef} - sim"]) %<<
+		legend(["\omega - mer","\omega - sml","i_{ef} - mer","i_{ef} - sml"]) %<<
 	end
 
 end
